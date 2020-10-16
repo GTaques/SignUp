ec1b81e3c43442d4ed11e06ac81b7965bafab085
gtaques <gabrielpctaques@gmail.com>
2020-10-16 17:37:51 -0300

    Initial Commit

---
+++ UserSignUp/UserSignUpApp.swift
@@ -0,0 +1,20 @@
+//
+//  UserSignUpApp.swift
+//  UserSignUp
+//
+//  Created by APPLE DEVELOPER ACADEMY on 16/10/20.
+//
+
+import SwiftUI
+
+@main
+struct UserSignUpApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
